2cb8e863d4d5975c2840d75b703ba32ef24eb749
Martin Meredith <martin@sourceguru.net>
2018-03-10 15:07:34 +0000

    Basic Status update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
+module.exports = robot => {
+  robot.log('Yay, the app was loaded!');
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  robot.on('push', async context => {
+    const { id: sha } = context.payload.head_commit;
+    robot.log(`Received Payload for sha:${sha}`);
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    context.github.repos.createStatus(
+      context.repo({
+        sha,
+        state: 'success',
+        target_url: 'https://cloche.mez.im',
+        description: 'Cloche is covering your branch',
+        context: 'cloche'
+      })
+    );
+  });
+};
